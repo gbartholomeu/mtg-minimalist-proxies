09b24164af21b9fd694b2bb09ce5ad1275d3b736
Gabriel Bartholomeu <gabriel_bartholomeu@hotmail.com>
2022-08-22 20:26:28 -0300

    Adding support for Phyrexian Mana

--- minimalistproxies/proxies.py
+++ minimalistproxies/proxies.py
@@ -7,87 +7,11 @@ import shutil
 import re
 import asyncio
 from pyppeteer import launch
+from .constants import *
+import time
 
 cache = Path(gettempdir()) / "scryfall_cache"
 cache.mkdir(parents=True, exist_ok=True)  # Create cache folder
-MANA_COST_MAPPING = {
-    '{W}': 'ms ms-w',
-    '{U}': 'ms ms-u',
-    '{B}': 'ms ms-b',
-    '{R}': 'ms ms-r',
-    '{G}': 'ms ms-g',
-    '{T}': 'ms ms-tap',
-    '{Q}': 'ms ms-untap',
-    '{E}': 'ms ms-e',
-    '{X}': 'ms ms-x',
-    '{Y}': 'ms ms-y',
-    '{Z}': 'ms ms-z',
-    '{0}': 'ms ms-0',
-    '{1}': 'ms ms-1',
-    '{2}': 'ms ms-2',
-    '{3}': 'ms ms-3',
-    '{4}': 'ms ms-4',
-    '{5}': 'ms ms-5',
-    '{6}': 'ms ms-6',
-    '{7}': 'ms ms-7',
-    '{8}': 'ms ms-8',
-    '{9}': 'ms ms-9',
-    '{10}': 'ms ms-10',
-    '{11}': 'ms ms-11',
-    '{12}': 'ms ms-12',
-    '{13}': 'ms ms-13',
-    '{14}': 'ms ms-14',
-    '{15}': 'ms ms-15',
-    '{16}': 'ms ms-16',
-    '{17}': 'ms ms-17',
-    '{18}': 'ms ms-18',
-    '{19}': 'ms ms-19',
-    '{20}': 'ms ms-20',
-    '{100}': 'ms ms-100',
-    '{1000000}': 'ms ms-1000000',
-    '{W/U}': 'ms ms-wu ms-cost bw',
-    '{W/B}': 'ms ms-wb ms-cost bw',
-    '{B/R}': 'ms ms-br ms-cost bw',
-    '{B/G}': 'ms ms-bg ms-cost bw',
-    '{U/B}': 'ms ms-ub ms-cost bw',
-    '{U/R}': 'ms ms-ur ms-cost bw',
-    '{R/G}': 'ms ms-rg ms-cost bw',
-    '{R/W}': 'ms ms-rw ms-cost bw',
-    '{G/W}': 'ms ms-gw ms-cost bw',
-    '{G/U}': 'ms ms-gu ms-cost bw',
-    '{W/P}': 'ms ms-wp ms-cost bw',
-    '{U/P}': 'ms ms-up ms-cost bw',
-    '{B/P}': 'ms ms-bp ms-cost bw',
-    '{R/P}': 'ms ms-rp ms-cost bw',
-    '{G/P}': 'ms ms-gp ms-cost bw',
-    '{HW}': 'ms ms-w ms-half',
-    '{HR}': 'ms ms-r ms-half',
-    '{S}': 'ms ms-s',
-    '{C}': 'ms ms-c'
-}
-
-PLANESWALKER_LOYALTY_MAPPING = {
-    'commom_css': 'ms',
-    'start_loyalty': 'ms-5x ms-loyalty-start',
-    'up_loyalty': 'ms-loyalty-up',
-    'down_loyalty': 'ms-loyalty-down',
-    'loyalty_amount': 'ms-2x ms-loyalty-'
-}
-
-MODAL_ICONS_MAPPING = {
-    'day': 'ms ms-dfc-day',
-    'night': 'ms ms-dfc-night',
-    'sparck': 'ms ms-dfc-spark',
-    'ignite': 'ms ms-dfc-ignite',
-    'enchantment': 'ms ms-dfc-enchantment',
-    'land': 'ms ms-land',
-    'moon': 'ms ms-dfc-moon',
-    'emrakul': 'ms ms-dfc-emrakul',
-    'modal_front': 'ms ms-dfc-modal-face',
-    'modal_back': 'ms ms-dfc-modal-back',
-    'lesson': 'ms ms-dfc-lesson'
-}
-
 
 def get_minimalist_proxies(card: Card, opposite_card: Card = [], full_card: Card = []) -> list[str]:
     file_name = 'minimalist_'
@@ -99,7 +23,7 @@ def get_minimalist_proxies(card: Card, opposite_card: Card = [], full_card: Card
 
     prepare_html_css(card, opposite_card, full_card)
     loop = asyncio.get_event_loop()
-    call = convert_html_to_png(f'{cache}/html_temp_{card["name"]}/html_template.html', str(cache / file_name))
+    call = convert_html_to_png(f'{cache}/html_temp/html_template.html', str(cache / file_name))
     loop.run_until_complete(call)
 
     return str(cache / file_name)
@@ -114,7 +38,7 @@ def prepare_html_css(card: Card, opposite_card: Card, full_card: Card):
 def prepare_files(card_name: str):
     # I'll keep this card_name target until I finish developing most cards layouts
     src = f'{os.getcwd()}/minimalistproxies/html'
-    dest = f'{cache}/html_temp_{card_name}'
+    dest = f'{cache}/html_temp'
 
     if os.path.exists(dest):
         shutil.rmtree(dest)
@@ -355,7 +279,7 @@ def replace_loyalty_counter(text: str) -> str:
     card_text = ''
 
     for line in text.splitlines():
-        loyalty = re.findall('^([−+])([0-9]{1,2})?(.+)', line)
+        loyalty = re.findall('^([−+])([0-9X]{1,2})?(.+)', line)
 
         if len(loyalty) == 0:
             card_text += f'<p>{line}</p>\n'
@@ -366,7 +290,7 @@ def replace_loyalty_counter(text: str) -> str:
             else:
                 css_loyalty += f' {PLANESWALKER_LOYALTY_MAPPING["down_loyalty"]}'
 
-            css_loyalty += f' {PLANESWALKER_LOYALTY_MAPPING["loyalty_amount"]}{loyalty[0][1]}'
+            css_loyalty += f' {PLANESWALKER_LOYALTY_MAPPING["loyalty_amount"]}{loyalty[0][1].lower()}'
             css_loyalty = f'<span class="{css_loyalty}"></span>'
 
             card_text += f'<p>{css_loyalty}{loyalty[0][2]}</p>\n'
@@ -375,11 +299,8 @@ def replace_loyalty_counter(text: str) -> str:
 
 
 def replace_text_symbols(text: str) -> str:
-    # card_text = ''
     loyalty_replaced = replace_loyalty_counter(text)
     mana_replaced = replace_mana_html(loyalty_replaced, False)
-    # for line in mana_replaced.splitlines():
-    #     card_text += f'<p>{line}</p>\n'
 
     return mana_replaced
 
